7855115519b07c211b97c40d673eab3b2586406f
yazawadesu <brengcomputacao@gmail.com>
2022-07-05 11:08:04 -0300

    Adicionar arquivos de projeto.

---
+++ WCconFinServer/Controllers/EstadoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WCconFinServer.Models;
+
+namespace WCconFinServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoController : ControllerBase
+    {
+
+        List<Estado> lista = new List<Estado>();
+
+        [HttpGet]
+        public List<Estado> GetEstado()
+        {
+            return lista;
+        }
+
+        [HttpPost]
+        public string PostEstado(Estado estado)
+        {
+            lista.Add(estado);
+            return "Estado cadastrado com sucesso!";
+        }
+    }
+}
---
+++ WCconFinServer/Models/Estado.cs
@@ -0,0 +1,8 @@
+﻿namespace WCconFinServer.Models
+{
+    public class Estado
+    {
+        public string Sigla { get; set;}
+        public string Nome { get; set;}
+    }
+}
